49ea9f785d941e95a8ec81cf28e84edd5fc01dae
ShawnBlaney <shawn.blaney@hotmail.ca>
2017-11-02 10:57:18 -0400

    migration and completed create user page

--- Mikes_Bikes/Models/Customer.cs
+++ Mikes_Bikes/Models/Customer.cs
@@ -3,16 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Mikes_Bikes.Models
 {
     public class Customer
     {
         public int CustomerID { get; set; }
+
+        [DisplayName("First Name")]
+        [Required(ErrorMessage = "First Name Required")]
         public string CustFName { get; set; }
+
+        [DisplayName("Last Name")]
+        [Required(ErrorMessage = "Last Name Required")]
         public string CustLName { get; set; }
+
+        [DisplayName("Email")]
+        [Required(ErrorMessage = "Email Required")]
         public string CustEmail { get; set; }
+
+        [DisplayName("Phone Number")]
+        [Required(ErrorMessage = "Phone Number Required")]
         public string CustPhone { get; set; }
+
+        [DisplayName("Password")]
+        [Required(ErrorMessage = "Password Required")]
         public string CustPwd { get; set; }
     }
 
