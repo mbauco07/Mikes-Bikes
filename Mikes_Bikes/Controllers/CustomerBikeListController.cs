d2bf758dcb5fe434f98214dc8a907c1be3e3b3a6
kabirvirdee <kabs27@hotmail.com>
2017-11-23 11:09:56 -0500

    Comparing start

--- Mikes_Bikes/Controllers/CustomerBikeListController.cs
+++ Mikes_Bikes/Controllers/CustomerBikeListController.cs
@@ -280,7 +280,7 @@ namespace Mikes_Bikes.Controllers
             int custId = 1;
             // This will have to be retrieved from the textbox on the ItemView page. 
 
-            int qty =0;
+            //int qty =0;
 
             int qty = q;
 
